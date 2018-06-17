c1e79f24bb23f301ec565a3297e0e179d225a4b6
Takashiro <takashiro@qq.com>
2018-06-17 14:06:53 +0800

    Add role button

---
+++ pages/room-creator/rolebutton.js
@@ -0,0 +1,34 @@
+// pages/room-creator/rolebutton.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    role: {
+      type: Number,
+      value: 0,
+    },
+    icon: {
+      type: String,
+      value: 'witch',
+    },
+    name: {
+      type: String,
+      value: 'Witch',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
