681090f0598d3c95a43cb4796f95131bfbd91a13
Takashiro <takashiro@qq.com>
2018-06-17 15:13:51 +0800

    Apply role config

--- pages/room-creator/index.js
+++ pages/room-creator/index.js
@@ -3,19 +3,33 @@ import Role from '../../game/Role';
 import Team from '../../game/Team';
 
 const selectors = [
-  { team: Team.Werewolf, basic: Role.Werewolf, roles: null },
-  { team: Team.Villager, basic: Role.Villager, roles: null },
+  { team: Team.Werewolf, basic: {role: Role.Werewolf, num: 0}, roles: null },
+  { team: Team.Villager, basic: {role: Role.Villager, num: 0}, roles: null },
   { team: Team.Other, basic: null, roles: null },
 ];
 for (const selector of selectors) {
   selector.roles = Role.List.filter(
-    role => role.team === selector.team && role !== selector.basic
+    role => role.team === selector.team && (!selector.basic || role !== selector.basic.role)
+  ).map(
+    role => ({role: role, num: 0})
   );
 }
 
 let roleConfig = new Map;
 
 function restoreRoleConfig() {
+  let configs = wx.getStorageSync('roleConfig');
+  if (!configs || !(configs instanceof Array)) {
+    roleConfig.set(Role.Werewolf, 4);
+    roleConfig.set(Role.Villager, 4);
+    roleConfig.set(Role.Seer, 4);
+    roleConfig.set(Role.Witch, 1);
+    roleConfig.set(Role.Hunter, 1);
+  } else {
+    for (let config of configs) {
+      roleConfig.set(config.role, config.num);
+    }
+  }
 }
 
 function saveRoleConfig() {
@@ -38,6 +52,27 @@ function saveRoleConfig() {
 Page({
   data: {
     selectors
+  },
+
+  onLoad: function (options) {
+    restoreRoleConfig();
+    let selectors = [];
+    for (let selector of this.data.selectors) {
+      if (selector.basic) {
+        let num = roleConfig.get(selector.basic.role.value);
+        selector.basic.num = num;
+      }
+
+      if (selector.roles) {
+        for (let role of selector.roles) {
+          let num = roleConfig.get(role.role.value);
+          role.num = isNaN(num) ? 0 : num;
+        }
+      }
+
+      selectors.push(selector);
+    }
+    this.setData({selectors: selectors});
   },
 
   handleRoleChange: function (e) {
