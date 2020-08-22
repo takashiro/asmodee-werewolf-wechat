a0204d8c3300ae63641ed7e1ac9e47835d63c7f7
Kazuichi Takashiro <takashiro@qq.com>
2020-08-22 10:12:03 +0800

    Add class Client

---
+++ test/base/Client.spec.ts
@@ -0,0 +1,46 @@
+import { client } from '../../src/base/Client';
+
+const rootUrl = 'https://werewolf.takashiro.cn/api';
+const request = jest.fn();
+
+beforeAll(() => {
+	Reflect.set(global, 'wx', {
+		request,
+	});
+});
+
+afterEach(() => {
+	request.mockClear();
+});
+
+it('sends a GET request', () => {
+	client.get({
+		url: 'room',
+		data: {
+			id: 1,
+		},
+	});
+	expect(request).toBeCalledWith({
+		method: 'GET',
+		url: `${rootUrl}/room`,
+		data: {
+			id: 1,
+		},
+	});
+});
+
+it('sends a POST request', () => {
+	client.post({
+		url: 'room',
+		data: {
+			id: 1,
+		},
+	});
+	expect(request).toBeCalledWith({
+		method: 'POST',
+		url: `${rootUrl}/room`,
+		data: {
+			id: 1,
+		},
+	});
+});
